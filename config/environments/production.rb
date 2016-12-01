a22e636bf23f1cc4eb82556d09c8c035199698d0
CodingRocks!! <ckhung5@gmail.com>
2016-12-01 17:37:24 +0800

    root_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options[:host] = ' https://kh-pairbnb.herokuapp.com/'
 
-config.action_mailer.default_url_options = { host: 'localhost:3000' } 
+
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.active_job.queue_name_prefix = "pairbnb"
